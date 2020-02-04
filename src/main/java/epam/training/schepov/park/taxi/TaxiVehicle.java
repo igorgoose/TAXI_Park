ffc0c91ed36f0d18623dacdbf2141af0f1e17d64
igorgoose <igorgood01@gmail.com>
2020-02-04 14:34:11 +0300

    Add getters to TaxiVehicle

--- src/main/java/epam/training/schepov/park/taxi/TaxiVehicle.java
+++ src/main/java/epam/training/schepov/park/taxi/TaxiVehicle.java
@@ -16,6 +16,23 @@ public abstract class TaxiVehicle {
         id = current_id++;
     }
 
+    public BigDecimal getPrice(){
+        return price;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public int getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public TaxiBrand getBrand() {
+        return brand;
+    }
 }
